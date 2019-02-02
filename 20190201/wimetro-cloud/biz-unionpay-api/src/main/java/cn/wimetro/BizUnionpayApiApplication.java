87169b8e847d144522824fa212af38bef5f43b89
wangwei <eric218@163.com>
2019-02-02 11:05:11 +0800

    add

---
+++ 20190201/wimetro-cloud/biz-unionpay-api/src/main/java/cn/wimetro/BizUnionpayApiApplication.java
@@ -0,0 +1,14 @@
+package cn.wimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizUnionpayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizUnionpayApiApplication.class, args);
+    }
+
+}
+
