38fda750b6c8b6146d6275083be35372ce4d8fb3
wangwei <eric218@163.com>
2019-02-02 10:05:42 +0800

    add

---
+++ 20190201/wimetro-cloud/base-common-api/src/main/java/cn/wimetro/BaseCommonApiApplication.java
@@ -0,0 +1,14 @@
+package cn.wimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class BaseCommonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseCommonApiApplication.class, args);
+    }
+
+}
+
