b0159f05947a5c42de0ac2c0dba26a3d57a75c6b
wangwei <eric218@163.com>
2019-02-02 14:05:40 +0800

    add

--- 20190201/wimetro-cloud/start-unionpay-server/src/test/java/cn/wimetro/StartUnionpayServerApplicationTests.java
+++ 20190201/wimetro-cloud/start-unionpay-server/src/test/java/cn/wimetro/StartUnionpayServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BaseCommonInterfaceApplicationTests {
+public class StartUnionpayServerApplicationTests {
 
     @Test
     public void contextLoads() {
