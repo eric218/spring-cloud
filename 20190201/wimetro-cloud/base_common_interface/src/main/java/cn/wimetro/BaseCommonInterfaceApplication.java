ea196db51a8503129ff51a9172cef9789b314b42
wangwei <eric218@163.com>
2019-02-02 10:40:25 +0800

    add

---
+++ 20190201/wimetro-cloud/base_common_interface/src/main/java/cn/wimetro/BaseCommonInterfaceApplication.java
@@ -0,0 +1,14 @@
+package cn.wimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class BaseCommonInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseCommonInterfaceApplication.class, args);
+    }
+
+}
+
