8eb11ad2af7633ac2866360da4927e293c38cc95
wangwei <eric218@163.com>
2019-02-13 14:48:22 +0800

    add

--- 20190201/wimetro-cloud/start-unionpay-server/src/main/java/cn/wimetro/controller/UnionPayConController.java
+++ 20190201/wimetro-cloud/start-unionpay-server/src/main/java/cn/wimetro/controller/UnionPayConController.java
@@ -2,8 +2,12 @@ package cn.wimetro.controller;
 
 import cn.wimetro.service.UnionPayConService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * 银联对外服务接口实现
@@ -15,10 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class UnionPayConController implements UnionPayConService {
-
+    @Value("${server.port}")
+    private String port ;
     @Override
     public String test1(@RequestParam("name") String name) {
-        log.info("UnionPayConController104:" + name);
-        return "UnionPayConController.test2:" + name;
+        String host = null;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress() + ":" + port;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        log.info(host + ": " + name);
+        return host + ": " + name;
     }
 }
